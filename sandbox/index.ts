8634c9b575ab37463b8ad4b279e82bf4ab8071f2
Andrii Klymenko <andrii.klymenko@underdefense.com>
2023-09-26 16:50:35 +0200

    handling case when object wasn't found in `HGETALL`

--- sandbox/index.ts
+++ sandbox/index.ts
@@ -1,5 +1,17 @@
 import 'dotenv/config';
 import { client } from '../src/services/redis';
 
-const run = async () => {};
+const run = async () => {
+    await client.hSet('car', {
+        color: 'red',
+        year: 1950
+    });
+
+    const car = await client.hGetAll('car123');
+    if (Object.keys(car).length === 0) { // if no object was found a {} will be returned
+        console.log('car not found');
+        return
+    }
+    console.log(car);
+};
 run();
